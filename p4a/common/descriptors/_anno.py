3fe30282deafbb4dd576e47b467a92e97f012463
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-09-20 12:34:50 +0000

    Fixed up zope 2.9 VS 2.10 compatibility.

--- p4a/common/descriptors/_anno.py
+++ p4a/common/descriptors/_anno.py
@@ -1,8 +1,8 @@
 from persistent.dict import PersistentDict
 try:
-    from zope.annotation import interfaces as annointerfaces
-except ImportError, e:
-    # zope 2.9/3.2 support
+    from zope.app.annotation import interfaces as annointerfaces
+except ImportError, err:
+    # Zope 2.10 support
     from zope.annotation import interfaces as annointerfaces
 
 _marker = object()
