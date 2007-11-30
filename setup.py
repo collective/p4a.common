9627072f1475d3cd9f2696bfdd70f3d8e14e33d1
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-11-30 17:25:33 +0000

    Updated dependencies.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(name='p4a.common',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          "setuptools",
       ],
       entry_points="""
       # -*- Entry points: -*-
