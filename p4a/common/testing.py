f4ab1850099647a4a11206ebcd239007e2c54111
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-12-03 16:59:02 +0000

    Fixed up MockSite to work with plone 2.5 and 3.0

--- p4a/common/testing.py
+++ p4a/common/testing.py
@@ -2,6 +2,16 @@ from zope import interface
 from zope.component.interfaces import ISiteManager
 from zope.app.component.interfaces import ISite
 
+import logging
+logger = logging.getLogger('p4a.common.testing')
+
+try:
+    import five.localsitemanager
+    HAS_FLSM = True
+    logger.info('Using five.localsitemanager')
+except ImportError, err:
+    HAS_FLSM = False
+
 class MockSite:
     """A simple ISite/ISiteManager combo for testing purposes.
     """
@@ -20,5 +30,9 @@ class MockSite:
     def getSiteManager(self):
         return self
 
-    def registerUtility(self, iface, obj):
-        self.utils[iface] = obj
+    if not HAS_FLSM:
+        def registerUtility(self, iface, obj):
+            self.utils[iface] = obj
+    else:
+        def registerUtility(self, obj, iface):
+            self.utils[iface] = obj
